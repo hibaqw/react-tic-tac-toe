ecf40f40acff06adc11985b41fbdc5a9087800bc
hibaqw <hibaqwarsame79@gmail.com>
2023-09-21 18:28:36 -0600

    implemented logic to declare winner of tictactoe game

--- src/App.js
+++ src/App.js
@@ -12,18 +12,25 @@ export default function Board() {
    */
   const [xIsNext, setXisNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
   function handleClick(i) {
     /*create copy of array
      * will use arrays to record game history
      * immutability
-     * */ 
+     * */
     const nextSquares = squares.slice();
     // if square is already filled exit out of function
     // can only mark empty squares
-    if (squares[i]){
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
-        // code to alternate turns between player X and player O
+    // code to alternate turns between player X and player O
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
@@ -35,8 +42,11 @@ export default function Board() {
     setXisNext(!xIsNext);
   }
 
+
+
   return (
     <div>
+      <div className='status'>{status}</div>
       <div className='board-row'>
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -53,4 +63,26 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </div>);
+}
+
+/**
+ * 
+ * @param {*} squares is an array containing the values of each square 
+ * on the tic tac toe gamee board
+ * calculates the winner of a single game of tic tac toe
+ * returns either 'X' or 'O' based on which player won that game
+ */
+function calculateWinner(squares) {
+  // array containing patterns of possible wins
+  const lines = [[0, 1, 2],
+  [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+
+  }
+  return null;
 }
